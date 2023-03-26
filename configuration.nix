63dcdc71cc2e438c1fa983d89461b98492c9b88b
adamstaveley <amstaveley94@gmail.com>
2023-03-26 22:54:27 +0200

    update boot loader config

--- configuration.nix
+++ configuration.nix
@@ -15,10 +15,10 @@ in
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.timeout = 120;
-  boot.loader.grub.configurationLimit = 5;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
